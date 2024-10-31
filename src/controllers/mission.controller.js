d823786603d6aa5a177edc93053209d997542e54
Ga Min Cha <prettygamin@naver.com>
2024-10-31 15:27:23 +0900

    refactor: #4 미션 도전하기 API 수정 (미션)

--- src/controllers/mission.controller.js
+++ src/controllers/mission.controller.js
@@ -1,24 +1,20 @@
 import { StatusCodes } from "http-status-codes";
-import { bodyToMission } from "../dtos/mission.dto.js";
+import { bodyToMission, bodyToMissionChallenge } from "../dtos/mission.dto.js";
 import { addMission } from "../services/mission.service.js";
 import { challengeMission } from "../services/missionChallenge.service.js";
 
 export const handleAddMission = async (req, res, next) => {
-    console.log("미션 추가를 요청했습니다!");
-    console.log("body:", req.body); // 값이 잘 들어오나 확인하기 위한 테스트용
+  console.log("미션 추가를 요청했습니다!");
+  console.log("body:", req.body); // 값이 잘 들어오나 확인하기 위한 테스트용
     
-    const mission = await addMission(bodyToMission(req.body));
-    res.status(StatusCodes.OK).json({ result: mission });
+  const mission = await addMission(bodyToMission(req.body));
+  res.status(StatusCodes.OK).json({ result: mission });
 };
 
 export const handleChallengeMission = async (req, res, next) => {
-    const { memberId, missionId } = req.body;
+  console.log("미션 도전 요청을 받았습니다!");
+  console.log("body:", req.body); // 값이 잘 들어오나 확인하기 위한 테스트용
   
-    try {
-      const result = await challengeMission(memberId, missionId);
-      res.status(StatusCodes.CREATED).json({ result });
-    } catch (error) {
-      console.error(error);
-      res.status(StatusCodes.BAD_REQUEST).json({ error: error.message });
-    }
-  };
+  const result = await challengeMission(bodyToMissionChallenge(req.body));
+  res.status(StatusCodes.OK).json({ result });
+};
