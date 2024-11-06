e894460581d4b448839c27794f6d1f2d6c10782a
Ga Min Cha <prettygamin@naver.com>
2024-11-06 21:20:21 +0900

    feat: DB migration (실습#2)

--- prisma/migrations/20241106121021_init_database/migration.sql
+++ prisma/migrations/20241106121021_init_database/migration.sql
@@ -1,4 +1,19 @@
 -- CreateTable
+CREATE TABLE `user` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `email` VARCHAR(255) NOT NULL,
+    `name` VARCHAR(100) NOT NULL,
+    `gender` VARCHAR(15) NOT NULL,
+    `birth` DATE NOT NULL,
+    `address` VARCHAR(255) NOT NULL,
+    `detail_address` VARCHAR(255) NULL,
+    `phone_number` VARCHAR(15) NOT NULL,
+
+    UNIQUE INDEX `email`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
 CREATE TABLE `food_category` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(100) NOT NULL,
