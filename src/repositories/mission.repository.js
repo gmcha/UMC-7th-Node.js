1ae24b319a2c3e49545982417afa094cde54db94
Ga Min Cha <prettygamin@naver.com>
2024-11-12 15:31:04 +0900

    bug: (실습#0)가게에 미션 추가하기 API

--- src/repositories/mission.repository.js
+++ src/repositories/mission.repository.js
@@ -14,7 +14,7 @@ export const addMissionToStore = async (data) => {
       },
     });
 
-    return mission.id;  // Prisma는 생성된 객체를 반환하므로, id 값을 반환
+    return mission;  // Prisma는 객체 형식으로 결과를 반환하므로 insertId와 같은 필드는 따로 반환하지 않아도 됩니다.
   } catch (err) {
     throw new Error(`오류가 발생했어요. 요청 파라미터를 확인해주세요. (${err.message})`);
   }
