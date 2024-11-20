532d69e137290fe974f23f9a6a356fb6e6cd425e
Ga Min Cha <prettygamin@naver.com>
2024-11-20 10:39:17 +0900

    Refactor: 에러 핸들링-(7)사용자가 진행 중인 미션 목록 API(실습)

--- src/services/mission.service.js
+++ src/services/mission.service.js
@@ -1,7 +1,8 @@
 import { responseFromMission, responseFromMissionList } from "../dtos/mission.dto.js";
 import { addMissionToStore, getAllStoreMissions, getAllUserMissions } from "../repositories/mission.repository.js";
-import { NonExistingStoreError } from "../errors.js";
+import { NonExistingStoreError, NonExistingMemberError } from "../errors.js";
 import { checkStoreExists } from "../repositories/store.repository.js";
+import { checkMemberExists } from '../repositories/user.repository.js';
 
 export const addMission = async (data) => {
   const storeExists = await checkStoreExists(data.storeId);
@@ -26,6 +27,12 @@ export const listStoreMissions = async (storeId, cursor) => {
 };
 
 export const listUserMissions = async (memberId, cursor) => {
+  const memberExists = await checkMemberExists(memberId);
+
+  if (!memberExists) {
+    throw new NonExistingMemberError("존재하지 않는 사용자입니다.");
+  }
+
   const missions = await getAllUserMissions(memberId, cursor);
   return responseFromMissionList(missions);
 };
